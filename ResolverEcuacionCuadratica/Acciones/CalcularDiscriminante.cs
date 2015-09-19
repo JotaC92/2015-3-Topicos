74cd2fe545c7b4208f713a8e36857f25b433dce0
JotaC92 <jotaceamador@gmail.com>
2015-09-19 16:28:17 -0600

    Helloworld y proyecto de Resolucion de Ecucaciones de Segundo Grado con Una Incognita

---
+++ ResolverEcuacionCuadratica/Acciones/CalcularDiscriminante.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResolverEcuacionCuadratica
+{
+    public class CalcularDiscriminante
+    {
+        public double CalcularElDiscriminante(double primerNumero, double SegundoNumero, double TercerNumero)
+        {
+            double resultado = 0;
+            resultado = (Math.Pow(SegundoNumero, 2) - 4 * primerNumero * TercerNumero);
+            return (resultado);
+        }
+    }
+}
---
+++ ResolverEcuacionCuadratica/Acciones/CalcularRaices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResolverEcuacionCuadratica
+{
+    public class CalcularRaices
+    {
+        public void CalcularLasRaices(double discriminante, double primerNumero, double segundoNumero, double tercerNumero, ref double primeraRaiz, ref double segundaRaiz)
+        {
+            primeraRaiz = (-primerNumero + Math.Sqrt(discriminante)) / (2 * primerNumero);
+            segundaRaiz = (-primerNumero - Math.Sqrt(discriminante)) / (2 * primerNumero);
+        }
+    }
+}
